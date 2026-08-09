476809692c955d965ce717baee3612d1dec0d02f
itsbusinessadmin <itsbusiness.admin@gmail.com>
2026-08-09 17:13:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
    The anon key is designed to be public in browser apps. Never put a service_role key here. */
 window.BILIHAN_CONFIG = {
   SUPABASE_URL: 'https://nrwiblxfqfovmnoofiii.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yd2libHhmcWZvdm1ub29maWlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzMDAwNDYsImV4cCI6MjEwMTg3NjA0Nn0.qlVPwylTwWgH1Tf4DdRiyxDHXBkirXhupKbobwVS4GI'
+  SUPABASE_ANON_KEY: 'sb_publishable_jCK3L5lQUejcSYQ4yau8sw_xLqUXanu'
 };
